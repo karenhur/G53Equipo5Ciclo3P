8657079ee9e30d58b4f2a76f168ed4d9a57151d8
karenhur <karenhurtado-12@hotmail.com>
2021-09-18 20:11:33 -0500

    commit
    Prueba

---
+++ tienda53/src/main/java/com/grupo53/tienda53/DAO/Conexion.java
@@ -0,0 +1,11 @@
+package com.grupo53.tienda53.DAO;
+
+public class Conexion {
+	
+	String nombre_bd="tiendagenerica";
+	String usuario_bd="root";
+	String password_bd="mintic";
+	String URL="jdbc:mysql://127.0.0.1/"+nombre_bd;
+	
+
+}
